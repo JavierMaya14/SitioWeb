424763dee4fba54924de6672b5e886fccf206aba
JavierMaya14 <106510124+JavierMaya14@users.noreply.github.com>
2024-02-14 00:30:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Gracias a Diosito, te amo mi negrita";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -44,10 +44,10 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "Baby piénsalo",
+    "Don't do this to me",
+    "Me woa morir",
+    "Te lo advertí",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
